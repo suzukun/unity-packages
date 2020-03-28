ddac51e56c2ea2722bf3495d06a8e70fd7a481a5
suzukun <suzuki-ryota@kayac.com>
2020-03-28 13:54:06 +0900

    init.

---
+++ Assets/__Tests__/Editor/EditModeSample.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Tests
+{
+    public class EditModeSample
+    {
+        [Test]
+        public void EditModeSampleSimplePasses()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
