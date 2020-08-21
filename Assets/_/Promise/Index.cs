b2fdaf37c823c10fd95ba48e91a8cdd6d2ce01bd
Ryota Suzuki <35280750+suzukun@users.noreply.github.com>
2020-08-21 16:34:29 +0900

    Update Index.cs

--- Assets/_/Promise/Index.cs
+++ Assets/_/Promise/Index.cs
@@ -1,21 +1,197 @@
-﻿namespace Promise
+namespace Promise
 {
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
+    using UniRx;
+    using System;
 
-    public class Index : MonoBehaviour
+    public class Promise <T>
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        public delegate T ChainReturnDelegate(T value);
+        public delegate void ChainDelegate(T value);
+        public delegate void ChainNoParamDelegate();
+        public delegate Promise<T> ChainPromiseDelegate(T value);
+        public delegate void ExecutorDelegate(ChainDelegate LocalResolve, ChainDelegate LocalReject);
+        public delegate void ExecutorNoParamDelegate(ChainNoParamDelegate LocalResolve, ChainNoParamDelegate LocalReject);
 
+        private AsyncSubject<T> _asyncSubject = new AsyncSubject<T>();
+        private object lockObject = new object();
+        private T _payload;
+        private float _delay = 0.0f;
+        private bool _isFulfilled = false;
+        private bool _isRejected = false;
+
+        public Promise(ExecutorDelegate Executor)
+        {
+            Executor(_Resolve, _Reject);
         }
 
-        // Update is called once per frame
-        void Update()
+        public Promise(ExecutorNoParamDelegate Executor)
         {
+            Executor(() => _Resolve(default(T)), () => _Reject(default(T)));
+        }
 
+        private Promise(ExecutorDelegate Executor, float delay)
+        {
+            _delay = delay;
+            Executor(_Resolve, _Reject);
+        }
+
+        private Promise(ExecutorNoParamDelegate Executor, float delay)
+        {
+            _delay = delay;
+            Executor(() => _Resolve(default(T)), () => _Reject(default(T)));
+        }
+
+        public static Promise<T> Reject()
+        {
+            return new Promise<T>((LocalResolve, LocalReject) => LocalReject());
+        }
+
+        public static Promise<T> Reject(T value)
+        {
+            return new Promise<T>((LocalResolve, LocalReject) => LocalReject(value));
+        }
+
+        public static Promise<T> Resolve()
+        {
+            return new Promise<T>((LocalResolve, LocalReject) => LocalResolve());
+        }
+
+        public static Promise<T> Resolve(T value)
+        {
+            return new Promise<T>((LocalResolve, LocalReject) => LocalResolve(value));
+        }
+
+        public static Promise<T> Wait(float delay)
+        {
+            return new Promise<T>((LocalResolve, LocalReject) => LocalResolve(), delay);
+        }
+
+        public static Promise<T> Wait(float delay, T value)
+        {
+            return new Promise<T>((LocalResolve, LocalReject) => LocalResolve(value), delay);
+        }
+
+        public Promise<T> Catch(ChainDelegate Chain)
+        {
+            return _Catch(_ => {
+                Chain(_payload);
+                return default(T);
+            });
+        }
+
+        public Promise<T> Catch(ChainReturnDelegate Chain)
+        {
+            return _Catch(Chain);
+        }
+
+        public Promise<T> Catch(ChainPromiseDelegate ChainPromise)
+        {
+            return ChainPromise(_payload);
+        }
+
+        public Promise<T> Then(ChainDelegate Chain)
+        {
+            return _Then(_ => {
+                Chain(_payload);
+                return default(T);
+            });
+        }
+
+        public Promise<T> Then(ChainReturnDelegate Chain)
+        {
+            return _Then(Chain);
+        }
+
+        public Promise<T> Then(ChainPromiseDelegate ChainPromise)
+        {
+            return ChainPromise(_payload);
+        }
+
+        private Promise<T> _Catch(ChainReturnDelegate Chain)
+        {
+            lock(lockObject)
+            {
+                return new Promise<T>((LocalResolve, LocalReject) => {
+                    try
+                    {
+                        _asyncSubject
+                            .First()
+                            .Where(_ => _isRejected)
+                            .Subscribe(_ => {
+                                _payload = Chain(_payload);
+                                LocalResolve(_payload);
+                            });
+
+                        _asyncSubject
+                            .First()
+                            .Where(_ => _isFulfilled)
+                            .Subscribe(_ => {
+                                LocalResolve(_payload);
+                            });
+                    }
+                    catch
+                    {
+                        LocalReject(_payload);
+                    }
+                });
+            }
+        }
+
+        private void _Reject(T value)
+        {
+            lock(lockObject)
+            {
+                if (_isFulfilled) return;
+
+                _isRejected = true;
+                _payload = value;
+                _asyncSubject.OnNext(value);
+                _asyncSubject.OnCompleted();
+            }
+        }
+
+        private void _Resolve(T value)
+        {
+            lock(lockObject)
+            {
+                if (_isRejected) return;
+
+                _isFulfilled = true;
+                _payload = value; 
+                _asyncSubject.OnNext(value);
+                _asyncSubject.OnCompleted();
+            }
+        }
+
+        private Promise<T> _Then(ChainReturnDelegate Chain)
+        {
+            lock(lockObject)
+            {
+                return new Promise<T>((LocalResolve, LocalReject) => {
+                    try
+                    {
+                        _asyncSubject
+                            .First()
+                            .Where(_ => _isFulfilled)
+                            .Delay(TimeSpan.FromMilliseconds(_delay))
+                            .Subscribe(_ => {
+                                _payload = Chain(_payload);
+                                LocalResolve(_payload);
+                            });
+
+                        _asyncSubject
+                            .First()
+                            .Where(_ => _isRejected)
+                            .Subscribe(_ => {
+                                LocalReject(_payload);
+                            });
+                    }
+                    catch
+                    {
+                        LocalReject(_payload);
+                    }
+                });
+            }
         }
     }
 }
